245f6b107c8c7b1c99468106a521de25e700bc50
Vega Fernando <fvega.3010@gmail.com>
2021-12-20 13:19:05 +0100

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,20 @@
+const dataFormName = document.getElementById('data-form')
+
+// dataFormName.addEventListener('submit',(e)=>{
+//     e.preventDefault()
+//     const dataName = document.getElementById('dataName').value.toUpperCase()
+//     const dataSurName = document.getElementById('dataSurname').value.toUpperCase()
+//     const dataEmail = document.getElementById('dataEmail').value.toLowerCase()
+//     const dataTextArea = document.getElementById('dataTextArea').value.toUpperCase()
+
+//     if(dataSurName.trim() || dataEmail.trim() || dataTextArea.trim() || dataName.trim() === '' ){
+//         alert('faltan datos')
+//     }
+
+//     console.log(dataName)
+//     console.log(dataSurName)
+//     console.log(dataEmail)
+//     console.log(dataTextArea)
+
+// } )
+
